4feab2fe583fdb9489b0073f64eb2785cb5c607f
arkalal <arkalal.123@gmail.com>
2023-07-08 00:07:44 +0530

    first push

---
+++ components/SignUp/SignUp.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./SignUp.module.scss";
+
+const SignUp = () => {
+  return <div>SignUp</div>;
+};
+
+export default SignUp;
---
+++ src/app/signup/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUp from "../../../components/SignUp/SignUp";
+
+const Signup = () => {
+  return (
+    <div>
+      <SignUp></SignUp>
+    </div>
+  );
+};
+
+export default Signup;
