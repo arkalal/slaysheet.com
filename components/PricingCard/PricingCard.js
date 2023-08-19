118695fd7b7b53803f3e53162ce11b0368e179f4
arkalal <arkalal.123@gmail.com>
2023-08-19 23:58:22 +0530

    no rule

--- components/PricingCard/PricingCard.js
+++ components/PricingCard/PricingCard.js
@@ -12,6 +12,7 @@ const PricingCard = ({ data }) => {
         <>
           <p> ✓ Get Good Features!!!</p>
           <p> ✓ Free access to all the AI Models</p>
+          <p>There are no rules</p>
         </>
       );
     }
