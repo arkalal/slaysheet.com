310724376af0ac9862aa30e5f6cf7ae75976ec58
arkalal <arkalal.123@gmail.com>
2023-09-26 21:49:55 +0530

    testing deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
