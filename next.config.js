273f0fc5b5db15fbef703ae7c117aa9ac7ab03ca
arkalal <arkalal.123@gmail.com>
2023-09-26 22:24:27 +0530

    deleted unused CRUD

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: false,
-  },
-};
+const nextConfig = {};
 
-export default nextConfig;
+module.exports = nextConfig;
