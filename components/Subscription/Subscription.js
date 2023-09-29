f57dd325e8f0f9059b3251775bfb9de96a179c9e
arkalal <arkalal.123@gmail.com>
2023-09-29 21:19:31 +0530

    fixed auth 2

--- components/Subscription/Subscription.js
+++ components/Subscription/Subscription.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Subscription.module.scss";
 import PricingCard from "../PricingCard/PricingCard";
 import { useRouter } from "next/navigation";
