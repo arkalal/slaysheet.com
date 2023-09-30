922c46cae33060daec1584c825a3fad130796040
arkalal <arkalal.123@gmail.com>
2023-09-30 14:57:59 +0530

    updated mongoDB code

--- utils/mongoDB.js
+++ utils/mongoDB.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URI);
+    await mongoose.connect(process.env.MONGO_DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("connected with MongoDB!!!!");
   } catch (error) {
     console.log(error);
