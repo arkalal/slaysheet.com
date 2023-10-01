895e135acef776da7e7f1c774e8a03c245fc7683
arkalal <arkalal.123@gmail.com>
2023-10-01 21:57:52 +0530

    testing

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -2,17 +2,17 @@ import React from "react";
 import Services from "../../../components/Services/Services";
 
 const Service = async () => {
-  // const userSession = await getServerSession(authOptions);
+  const userSession = await getServerSession(authOptions);
 
-  // const subscribedData = await UserSubscription.findOne({
-  //   user: userSession?.user.email,
-  // });
+  const subscribedData = await UserSubscription.findOne({
+    user: userSession?.user.email,
+  });
 
   return (
     <div>
       <Services
-      // subscribedId={subscribedData?._id}
-      // isSubscribed={subscribedData ? true : false}
+        subscribedId={subscribedData?._id}
+        isSubscribed={subscribedData ? true : false}
       ></Services>
       {/* <p>services</p> */}
     </div>
