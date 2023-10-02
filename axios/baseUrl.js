38c2e359ecd4fabfdca291f3d3d291940220d528
arkalal <arkalal.123@gmail.com>
2023-10-03 01:14:03 +0530

    testing mistake

--- axios/baseUrl.js
+++ axios/baseUrl.js
@@ -1,4 +1,4 @@
 export const baseUrlTest = "http://localhost:3000";
-export const baseUrlStaging = "https://ai-app-official-arkalal.vercel.app/";
+export const baseUrlStaging = "https://ai-app-official-arkalal.vercel.app";
 export const baseUrlProd =
   "https://ai-app-official-6htoe7vip-arkalal.vercel.app";
