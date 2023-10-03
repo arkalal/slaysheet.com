75d7f32332d9a43934750288753e9728c28b454e
arkalal <arkalal.123@gmail.com>
2023-10-03 23:13:32 +0530

    revert abck

--- src/app/(root)/(routes)/services/page.js
+++ src/app/(root)/(routes)/services/page.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Services from "../../../../../components/Services/Services";
-import checkSubscription from "../../../../../utils/checkSubscription";
+import axios from "../../../../../axios/getApi";
+
+const getWebhook = async () => {
+  const res = await axios.get("webhook");
+  return res.data;
+};
 
 const Service = async () => {
-  const isPro = await checkSubscription();
+  const webhook = await getWebhook();
+  console.log("webhook", webhook);
 
   return (
     <div>
       <Services
-        // subscribedId={data && data?._id}
-        isSubscribed={isPro}
+      // subscribedId={data && data?._id}
+      // isSubscribed={webhook ? true : false}
       ></Services>
     </div>
   );
