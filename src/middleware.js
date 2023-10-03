b6c609856dd7d0133a18b0910e058555b28c4b3f
arkalal <arkalal.123@gmail.com>
2023-10-03 16:09:40 +0530

    protecting route

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,17 @@
-import { withClerkMiddleware } from "@clerk/nextjs";
+import { withClerkMiddleware, authMiddleware } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default withClerkMiddleware(() => {
-  //console.log("middleware running...");
-  return NextResponse.next();
+
+// export default withClerkMiddleware(() => {
+//   //console.log("middleware running...");
+//   return NextResponse.next();
+// });
+
+export default authMiddleware({
+  publicRoutes: ["/"],
 });
 
 export const config = {
