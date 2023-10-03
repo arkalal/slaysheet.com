17e6206856e6771ff11418c6431aaefd77b7527d
arkalal <arkalal.123@gmail.com>
2023-10-03 23:45:55 +0530

    testing staging

--- src/app/(root)/(routes)/services/page.js
+++ src/app/(root)/(routes)/services/page.js
@@ -1,21 +1,36 @@
 import React from "react";
 import Services from "../../../../../components/Services/Services";
 import axios from "../../../../../axios/getApi";
+import { currentUser } from "@clerk/nextjs";
 
 const getWebhook = async () => {
   const res = await axios.get("webhook");
   return res.data;
 };
 
-const Service = async () => {
+const checkSubscription = async () => {
+  const user = await currentUser();
   const webhook = await getWebhook();
-  console.log("webhook", webhook);
+
+  const isSubscribed = Array.from(webhook.subscription).some(
+    (item) => item.user === user.emailAddresses[0].emailAddress
+  );
+
+  if (isSubscribed) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const Service = async () => {
+  const isPro = await checkSubscription();
 
   return (
     <div>
       <Services
-      // subscribedId={data && data?._id}
-      // isSubscribed={webhook ? true : false}
+        // subscribedId={data && data?._id}
+        isSubscribed={isPro}
       ></Services>
     </div>
   );
