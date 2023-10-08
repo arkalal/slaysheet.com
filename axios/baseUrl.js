cb43aed9e377aca1f6d09bcef8a1f1aed586dfac
arkalal <arkalal.123@gmail.com>
2023-10-08 16:54:23 +0530

    testing prod

--- axios/baseUrl.js
+++ axios/baseUrl.js
@@ -1,4 +1,4 @@
 export const baseUrlTest = "http://localhost:3000";
-export const baseUrlStaging = "ai-app-git-master-arkalal.vercel.app";
+export const baseUrlStaging = "https://ai-app-theta.vercel.app";
 export const baseUrlProd =
   "https://ai-app-official-6htoe7vip-arkalal.vercel.app";
