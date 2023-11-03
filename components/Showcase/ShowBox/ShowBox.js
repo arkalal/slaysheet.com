53d178a528d18988b1694111315e7ef5d98a7462
arkalal <arkalal.123@gmail.com>
2023-11-04 00:53:01 +0530

    optimized dynamic rendering

--- components/Showcase/ShowBox/ShowBox.js
+++ components/Showcase/ShowBox/ShowBox.js
@@ -1,13 +1,35 @@
 import React from "react";
 import ShowScreens from "./ShowScreens/ShowScreens";
 import styles from "./ShowBox.module.scss";
-import AIChat from "../ShowComp/AIChat/AIChat";
-import AutoEmail from "../ShowComp/AutoEmail/AutoEmail";
-import CreateNote from "../ShowComp/CreateNote/CreateNote";
-import GenImages from "../ShowComp/GenImages/GenImages";
-import GenMusic from "../ShowComp/GenMusic/GenMusic";
-import PDFChat from "../ShowComp/PDFChat/PDFChat";
-import MyNotes from "../ShowComp/MyNotes/MyNotes";
+import dynamic from "next/dynamic";
+
+const PDFChat = dynamic(() => import("../ShowComp/PDFChat/PDFChat"), {
+  ssr: true,
+});
+
+const AutoEmail = dynamic(() => import("../ShowComp/AutoEmail/AutoEmail"), {
+  ssr: true,
+});
+
+const AIChat = dynamic(() => import("../ShowComp/AIChat/AIChat"), {
+  ssr: true,
+});
+
+const GenImages = dynamic(() => import("../ShowComp/GenImages/GenImages"), {
+  ssr: true,
+});
+
+const CreateNote = dynamic(() => import("../ShowComp/CreateNote/CreateNote"), {
+  ssr: true,
+});
+
+const GenMusic = dynamic(() => import("../ShowComp/GenMusic/GenMusic"), {
+  ssr: true,
+});
+
+const MyNotes = dynamic(() => import("../ShowComp/MyNotes/MyNotes"), {
+  ssr: true,
+});
 
 const ShowBox = () => {
   const showScreenComp = {
