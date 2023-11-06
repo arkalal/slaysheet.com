e198335adade61072ea0fd347844d5ab009bcbf9
arkalal <arkalal.123@gmail.com>
2023-11-06 20:14:16 +0530

    moving to prod

--- axios/openAiApi.js
+++ axios/openAiApi.js
@@ -3,7 +3,7 @@ import { baseUrlProd, baseUrlStaging, baseUrlTest } from "./baseUrl";
 import { openAiApiKey } from "./constant";
 
 const instance = axios.create({
-  baseURL: `${baseUrlTest}/api/`,
+  baseURL: `${baseUrlProd}/api/`,
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${openAiApiKey}`,
