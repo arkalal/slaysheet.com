a6bc486444a83e60332f655861b97aac52238eb4
arkalal <arkalal.123@gmail.com>
2023-11-16 23:49:34 +0530

    moving opt to prod

--- axios/getApi.js
+++ axios/getApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { baseUrlProd, baseUrlStaging, baseUrlTest } from "./baseUrl";
 
 const instance = axios.create({
-  baseURL: `${baseUrlStaging}/api/`,
+  baseURL: `${baseUrlProd}/api/`,
   headers: {
     "Content-Type": "application/json",
   },
--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -35,8 +35,8 @@ export async function POST(req) {
     ],
     customer_email: userSession.user.email,
     mode: "subscription",
-    success_url: `${baseUrlStaging}/studio`,
-    cancel_url: `${baseUrlStaging}/studio`,
+    success_url: `${baseUrlProd}/studio`,
+    cancel_url: `${baseUrlProd}/studio`,
     billing_address_collection: "auto",
     metadata: {
       userId: userSession.user.email,
