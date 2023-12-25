23c9389c13eaf060ba9411350f661cbf908b2335
arkalal <arkalal.123@gmail.com>
2023-12-25 19:59:19 +0530

    created pricing page

--- components/Screens/PricingScreen/PricingCard/PricingCard.js
+++ components/Screens/PricingScreen/PricingCard/PricingCard.js
@@ -9,14 +9,15 @@ import { useSession } from "next-auth/react";
 const PricingCard = ({ data }) => {
   const { data: session } = useSession();
 
-  console.log("session", session);
-
   const dynamicPricingCardData = (name) => {
-    if (name === "Basic Individual - AI") {
+    if (name === "AI Tokens Plan") {
       return (
         <>
-          <p> ✓ Get Good Features!!!</p>
-          <p> ✓ Free access to all the AI Models</p>
+          <p> ✓ 50 AI Tokens</p>
+          <p> ✓ Full Access for All AI tools</p>
+          <p> ✓ Join Discord Community</p>
+          <p> ✓ Early Access Perks</p>
+          <p> ✓ What Feature You Want? We Will Build It</p>
         </>
       );
     }
@@ -27,38 +28,49 @@ const PricingCard = ({ data }) => {
   const handleSubscription = async (e) => {
     e.preventDefault();
 
+    if (!session) {
+      router.push("/login");
+    }
+
     const postData = {
       priceId: data.id,
       mode: data.type,
       productId: data.product,
-      userId: session.user.email,
+      userId: session?.user.email,
     };
 
     try {
-      const res = await axios.post("checkout", postData);
-      router.push(res.data);
+      if (session) {
+        const res = await axios.post("checkout", postData);
+        router.push(res.data);
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <div>
-      <h3> {data.nickname} </h3>
-      <div>{dynamicPricingCardData(data.nickname)}</div>
+    <>
+      <div className={styles.PricingCard}>
+        <h3> {data.nickname} </h3>
+        <div className={styles.pricingFeatures}>
+          {dynamicPricingCardData(data.nickname)}
+        </div>
 
-      <br />
+        <br />
 
-      <h2>
-        {" "}
-        {(data.unit_amount / 100).toLocaleString("en-US", {
-          style: "currency",
-          currency: "INR",
-        })}{" "}
-      </h2>
+        <h2>
+          {" "}
+          {(data.unit_amount / 100).toLocaleString("en-US", {
+            style: "currency",
+            currency: "INR",
+          })}{" "}
+        </h2>
+        <p>One Time Pay</p>
 
-      <button onClick={handleSubscription}>Buy Now</button>
-    </div>
+        <button onClick={handleSubscription}>Buy Now</button>
+      </div>
+    </>
   );
 };
 
