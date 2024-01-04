eddce14edd98f2ee57902154f016468cfe34b63c
arkalal <arkalal.123@gmail.com>
2024-01-04 12:22:29 +0530

    done with footer responsive

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,22 +5,28 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className={styles.Footer}>
-      <Link className={styles.footerLink} href="/privacyPolicy">
-        {" "}
-        <p>Privacy Policy</p>{" "}
-      </Link>
-      <Link className={styles.footerLink} href="/termsOfService">
-        {" "}
-        <p>Terms Of Service</p>{" "}
-      </Link>
-      <Link className={styles.footerLink} href="/pricing">
-        {" "}
-        <p>Pricing</p>{" "}
-      </Link>
-      <Link className={styles.footerLink} href="/contactUs">
-        {" "}
-        <p>Contact Us</p>{" "}
-      </Link>
+      <div className={styles.footerNav}>
+        <Link className={styles.footerLink} href="/privacyPolicy">
+          {" "}
+          <p>Privacy Policy</p>{" "}
+        </Link>
+        <Link className={styles.footerLink} href="/termsOfService">
+          {" "}
+          <p>Terms Of Service</p>{" "}
+        </Link>
+        <Link className={styles.footerLink} href="/pricing">
+          {" "}
+          <p>Pricing</p>{" "}
+        </Link>
+        <Link className={styles.footerLink} href="/contactUs">
+          {" "}
+          <p>Contact Us</p>{" "}
+        </Link>
+      </div>
+
+      <div className={styles.footerBase}>
+        <p>© 2024 Slaysheet, Inc</p>
+      </div>
     </div>
   );
 };
